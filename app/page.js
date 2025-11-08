2c5774d7388ba6bcb85697b27f7cdbfa0bfe88d4
Johan Piyabordin <kaiju5678@gmail.com>
2025-11-08 16:06:50 +0700

    Add Alert

--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
 "use client"
-import { useEffect, useState } from 'react';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-
+import { Alert } from 'reactstrap';
 
 export default function Button() {
   const router = useRouter();
   return (
 
-    <div style={{ backgroundColor: '#fffafa' }}>
-      <div style={{ color: '#318d51' }}>
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 'auto',
+      minHeight: '100vh',
+      backgroundColor: '#fffafa',
+      flexDirection: 'column',
+    }}>
+      <Alert color="primary">Danger</Alert>
+      <button onClick={() => router.push('/another-page')}>Go to Another Page</button>
+      <div style={{
+        color: '#318d51',
+        fontWeight: 700,
+      }}>
         Text
+      </div>
+      <div className="text-3xl p-3 bg-red-500 rounded-[50px] text-yellow-500">
+        Hello Tailwind CSS
       </div>
     </div>
   );
